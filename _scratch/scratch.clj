0ec3a87347e909d7843aecef5d6b0c838ff865bb
Abhinav Sharma <abhi18av@gmail.com>
2018-11-30 18:54:36 +0530

    installed the fluidr3 soundfont

--- _scratch/scratch.clj
+++ _scratch/scratch.clj
@@ -1,2 +1,34 @@
 (ns musicxml.scratch)
 
+
+(require '[alda.lisp :refer :all])
+
+(require '[alda.now  :as    now])
+
+
+(now/play!
+ (part "piano"
+       (note (pitch :c) (duration (note-length 8)))
+       (note (pitch :d))
+       (note (pitch :e :flat))
+       (note (pitch :f))
+       (note (pitch :g))
+       (note (pitch :a :flat))
+       (note (pitch :b))
+       (octave :up)
+       (note (pitch :c))))
+
+
+;; soundfont
+(require '[midi.soundfont :refer (load-all-instruments! midi-test load-patch)]
+         '[midi.soundfont.fluid-r3 :as fluid-r3])
+
+(import '(javax.sound.midi MidiSystem))
+
+(def synth (MidiSystem/getSynthesizer))
+(load-all-instruments! synth fluid-r3/sf2)
+
+(midi-test synth) ; you should hear a nice-sounding piano
+
+(load-patch synth 30)
+(midi-test synth) ; you should hear a gnarly distorted guitar!
--- project.clj
+++ project.clj
@@ -3,4 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+
+
+                 ;; Alda libraries
+                 [alda/core             "0.3.10"]
+                 [alda/sound-engine-clj "1.0.0"]
+
+                 ;; Soundfonts
+
+                 [midi.soundfont "0.1.0"]
+                 [org.bitbucket.daveyarwood/fluid-r3 "0.1.1"]
+
+                 ])
